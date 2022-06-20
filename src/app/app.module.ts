43f47bb87b8f55f9fde19c44b5e9fac95d276c23
SaqlainEccountant <101181101+SaqlainEccountant@users.noreply.github.com>
2022-06-20 10:40:00 +0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AbcComponent} from "./components/abc.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AbcComponent,
+    LoginComponent,
+    UserListComponent,
 
 
   ],
